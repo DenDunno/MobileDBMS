3db07a511226f0d82921675f4c61917a38e9098b
DenDunno <60221683+DenDunno@users.noreply.github.com>
2022-10-16 17:15:04 +0300

    initial commit

---
+++ Assets/Code/TableResizement.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UI.TableUI;
+
+public class TableResizement : MonoBehaviour
+{
+    [SerializeField] private TableUI _tableUI;
+    [SerializeField] private VerticalLayoutGroup _verticalLayout;
+    
+    
+}
