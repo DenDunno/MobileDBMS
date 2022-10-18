19f04b3d207429b98d3d75be8f245931be8d9176
Eyoushkov <60221683+DenDunno@users.noreply.github.com>
2022-10-18 18:58:42 +0300

    table preview start

--- Assets/SimpleTableUI/Scripts/UI/UI.cs
+++ Assets/SimpleTableUI/Scripts/UI/UI.cs
@@ -2,18 +2,21 @@ using UnityEngine;
 
 public class UI : MonoBehaviour
 {
+    [SerializeField] private TablePreview _tablePreview;
     [SerializeField] private TablePanel _tablePanel;
     [SerializeField] private ColumnAddingPanel _columnAddingPanel;
     private Panel[] _uiElements;
 
     private void Start()
     {
-        _uiElements = new Panel[] {_tablePanel, _columnAddingPanel};
+        _uiElements = new Panel[] {_tablePanel, _columnAddingPanel, _tablePreview};
     }
 
     public void ShowTable() => ShowPanel(_tablePanel);
 
     public void ShowColumnAddingPanel() => ShowPanel(_columnAddingPanel);
+    
+    public void ShowTablePreview() => ShowPanel(_tablePreview);
 
     private void ShowPanel(Panel panel)
     {
